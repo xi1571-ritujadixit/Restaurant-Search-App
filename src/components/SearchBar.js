d920a09772eade9e0d570ff641cdcb08837a711f
Rituja Dixit <rituja.dixit@xebia.com>
2021-10-29 12:08:10 +0530

    managing state

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,11 +2,18 @@ import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange }) => {
     return (
         <View style={styles.backgroundStyle}>
-            <Feather name="search" size={30} />
-            <TextInput placeholder="Search" style={styles.inputStyle} />
+            <Feather name="search" style={styles.iconStyle} />
+            <TextInput
+                autoCapitalize="none"
+                autoCorrect={false}
+                placeholder="Search"
+                style={styles.inputStyle}
+                value={term}
+                onChangeText={(newTerm) => onTermChange(newTerm)}
+            />
         </View>
     );
 };
@@ -17,12 +24,17 @@ const styles = StyleSheet.create({
         height: 50,
         borderRadius: 5,
         marginHorizontal: 15,
-        flexDirection: 'row'
+        flexDirection: 'row',
+        marginTop: 10
     },
     inputStyle: {
-        borderColor: 'black',
-        borderWidth: 1,
-        flex: 1
+        flex: 1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 35,
+        alignSelf: 'center',
+        marginHorizontal: 15
     }
 });
 
